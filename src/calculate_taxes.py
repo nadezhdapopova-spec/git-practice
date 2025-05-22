0312b8229faff1cff00e512725522234dc2ed4da
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-05-22 21:02:26 +0700

    modify func calculate_tax with discount and round and add new tests

--- src/calculate_taxes.py
+++ src/calculate_taxes.py
@@ -15,10 +15,18 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     return taxed_prices
 
 
-def calculate_tax(price: float, tax_rate: float) -> float:
+def calculate_tax(price: float, tax_rate: float, *, discount: int=0, rounder: int=2) -> float:
     """Вычисляет стоимость одного товара с учётом налога."""
+
+    for arg in [price, tax_rate, discount, rounder]:
+        if not isinstance(arg, (int | float)):
+            raise TypeError("Ошибка типа данных")
+
     if price <= 0:
         raise ValueError("Неверная цена")
     if tax_rate < 0 or tax_rate >= 100:
         raise ValueError("Неверный налоговый процент")
-    return round(price + (price * tax_rate / 100), 2)
+
+    new_price = price + price * tax_rate / 100
+
+    return round(new_price - new_price * discount / 100, rounder)
